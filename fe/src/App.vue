2440eb29fed91d86892763c2574e8c2e90260218
fungleo <web@fengcms.com>
2023-07-31 21:35:33 +0800

    opt code

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,11 +1,7 @@
-<template>
-  <div id="app">
-    <router-view :name="$router.path" />
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'App'
-}
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
 </script>
+
+<template>
+  <RouterView />
+</template>
